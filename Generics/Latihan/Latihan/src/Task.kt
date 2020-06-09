fbb746ae07c9c0522010928b9a661562be4befc1
rzkinr <rizkynurrokhim18@gmail.com>
2020-06-09 21:27:27 +0700

    first

---
+++ Generics/Latihan/Latihan/src/Task.kt
@@ -0,0 +1,22 @@
+/**
+ * Untuk menyelesaikan tugas latihan, Anda tidak diperbolehkan mengubah kode yang sudah ada.
+ * Cukup tambahkan kode berdasarkan perintah yang sudah ditentukan.
+ *
+ */
+fun main() {
+    val stringResult = getResult("Kotlin")
+    val intResult = getResult(100)
+
+    // TODO 2
+    println(stringResult)
+    println(intResult)
+}
+
+// TODO 1
+fun <T> getResult(args: T): Int {
+    return when(args){
+        is Int -> args * 5
+        is String -> args.length
+        else -> 0
+    }
+}
---
+++ Kotlin Fundamental/Latihan/Latihan/src/Task.kt
@@ -0,0 +1,25 @@
+/**
+ * Untuk menyelesaikan tugas latihan, Anda tidak diperbolehkan mengubah kode yang sudah ada.
+ * Cukup tambahkan kode berdasarkan perintah yang sudah ditentukan.
+ *
+ */
+fun main() {
+    val valueA = 101
+    val valueB = 52
+    val valueC = 99
+
+    val resultA = calculateResult(valueA, valueB, valueC)
+    val resultB = calculateResult(valueA, valueB, null)
+
+    println("""
+        ResultA is $resultA
+        ResultB is $resultB
+    """.trimIndent())
+}
+
+fun calculateResult(valueA: Int, valueB: Int, valueC: Int?): Int {
+    // TODO
+    val nullC = valueC?: 50
+    val result = valueA + (valueB - nullC)
+    return result
+}
